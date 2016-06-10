cafa05b51c50d551afcf42bd6ae26ae309c47a5e
Jesse Allers-Hatlie <jallershatlie@gmail.com>
2016-06-09 21:05:16 -0400

    Add project files.

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiExampleRC2.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+
+        /// <summary>
+        /// New example api endpoint that returns a int.
+        /// </summary>
+        /// <remarks>Pass in a int and this method will add 10 to it and then return it.</remarks>
+        /// <param name="valueA"></param>
+        /// <returns></returns>
+        [HttpGet("ExampleGet/{valueA:int}")]    //inline constraints - route matching does NOT happen if valueA is not a int.
+        [Produces(typeof(int))]                 //adds model and schema to swaggerm very helpful for passing around objects. 
+        [Consumes("application/json")]          //Tell this action to only accecpt json objects
+        public IActionResult ExampleGet(int valueA)
+        {
+            try
+            {
+                valueA = valueA + 10;
+                return this.Ok(valueA);
+            }
+            catch (Exception)
+            {
+                return this.BadRequest("Fail!");
+            }
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Swashbuckle.SwaggerGen.Generator;
+
+namespace ApiExampleRC2
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            string pathToDoc = "C:\\WorkSpace\\GitHub\\ApiExampleRC2\\bin\\Debug\\netcoreapp1.0\\ApiExampleRC2.xml";
+
+            services.AddMvc();
+
+            services.AddSwaggerGen();
+            services.ConfigureSwaggerGen(options =>
+            {
+                options.SingleApiVersion(new Info
+                {
+                    Version = "v1",
+                    Title = "Example API",
+                    Description = "An API API With Swagger for RC2",
+                    TermsOfService = "None"
+                });
+                options.IncludeXmlComments(pathToDoc);
+                options.DescribeAllEnumsAsStrings();
+            });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseMvc();
+
+            app.UseSwaggerGen();
+            app.UseSwaggerUi();
+        }
+    }
+}
